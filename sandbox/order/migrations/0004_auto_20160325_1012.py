df6f81c577550566641c20cadb056f3ee83af80f
Craig Weber <crgwbr@gmail.com>
2018-01-02 14:19:20 -0500

    fix django deprecation warnings

--- sandbox/order/migrations/0004_auto_20160325_1012.py
+++ sandbox/order/migrations/0004_auto_20160325_1012.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='line',
             name='basket_line',
-            field=models.OneToOneField(to='basket.Line', null=True, related_name='order_line'),
+            field=models.OneToOneField(to='basket.Line', null=True, related_name='order_line', on_delete=models.CASCADE),
         ),
         migrations.AddField(
             model_name='order',
