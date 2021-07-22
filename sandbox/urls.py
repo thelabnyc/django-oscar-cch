d67c6c684110016ad346548a5d8d8ff3fdfbdb14
Craig Weber <crgwbr@gmail.com>
2021-07-22 11:04:44 -0400

    Refs #17848 Fix deprecation warnings

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,12 +1,13 @@
 from django.apps import apps
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include
+from django.urls import re_path
 from django.contrib import admin
 
 
 urlpatterns = patterns(
     "",
-    url(r"^i18n/", include("django.conf.urls.i18n")),
-    url(r"^admin/", admin.site.urls),
-    url(r"^api/", include("oscarapi.urls")),
-    url(r"^", include(apps.get_app_config("oscar").urls[0])),
+    re_path(r"^i18n/", include("django.conf.urls.i18n")),
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^api/", include("oscarapi.urls")),
+    re_path(r"^", include(apps.get_app_config("oscar").urls[0])),
 )
